b060b22d306d8732130d0b56227f13aa3cd963af
Tommaso Cesaro <to.cesaro@studenti.unina.it>
2025-02-24 09:58:23 +0100

    Start

---
+++ epicEnergyServices/src/test/java/com/epicEnergyServices/EpicEnergyServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicEnergyServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
